56c28a26bdbcd7f9b40dfd0f9c2466d2c79456bc
Paulo Freire <paulohtfreire@gmail.com>
2026-01-10 21:43:07 -0300

    fix input with sscanf and fgets

--- client/client.c
+++ client/client.c
@@ -1,11 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void){
-    char input[255];
-    while(1){
+int main(void) {
+    char input[300];
+    char command[8];
+    char key[37];
+    char item[257];
+    int ttl = 0;
+
+    while (1) {
         printf("Digite um comando:\n");
-        fgets(input, 255, stdin);
-        printf("Comando digitado  \"%s\"\n", input);
+
+        if (!fgets(input, sizeof(input), stdin)) {
+            break;
+        }
+
+        if (sscanf(input, "%7s", command) != 1) {
+            printf("Comando inválido\n");
+            continue;
+        }
+
+        if (strcmp(command, "PUT") == 0) {
+            if (sscanf(input, "PUT \"%127[^\"]\" -t %d", item, &ttl) == 2) {
+                printf("CMD -> PUT | ITEM -> \"%s\" | TTL -> %d\n", item, ttl);
+            } else {
+                printf("Formato inválido para PUT\n");
+            }
+        }
+        else if (strcmp(command, "GET") == 0 || strcmp(command, "DEL") == 0) {
+            if (sscanf(input, "%*s -k \"%37[^\"]\"", key) == 1) {
+                printf("CMD -> %s | KEY -> \"%s\"\n", command, key);
+            } else {
+                printf("Formato inválido para %s\n", command);
+            }
+        }
+        else {
+            printf("Comando desconhecido: %s\n", command);
+        }
     }
+
     return 0;
 }
